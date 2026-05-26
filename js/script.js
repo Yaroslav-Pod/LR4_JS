05a844048856211933e3e9b39a25456878fc8a0d
Yaroslav-Pod <yrslav.pod@gmail.com>
2026-05-26 22:13:28 +0300

    Add type conversion examples and template string usage in script.js to task1

--- js/script.js
+++ js/script.js
@@ -30,10 +30,33 @@ const symVar = Symbol("id");
 const bigIntVar = 9007199254740991n;
 
 logResult(`Примітиви та їх типи:
-"${strVar}" — тип: ${typeof strVar}
-${numVar} — тип: ${typeof numVar}
-${boolVar} — тип: ${typeof boolVar}
-${nullVar} — тип: ${typeof nullVar}
-${undefVar} — тип: ${typeof undefVar}
-${symVar.toString()} — тип: ${typeof symVar}
-${bigIntVar} — тип: ${typeof bigIntVar}`);
+    "${strVar}" — тип: ${typeof strVar}
+    ${numVar} — тип: ${typeof numVar}
+    ${boolVar} — тип: ${typeof boolVar}
+    ${nullVar} — тип: ${typeof nullVar}
+    ${undefVar} — тип: ${typeof undefVar}
+    ${symVar.toString()} — тип: ${typeof symVar}
+    ${bigIntVar} — тип: ${typeof bigIntVar}`);
+// Явне перетворення
+logResult(`Явне перетворення типів:
+    String(100): ${String(100)} (${typeof String(100)})
+    Number("555"): ${Number("555")} (${typeof Number("555")})
+    Number("abc"): ${Number("abc")} (${typeof Number("abc")})
+    Boolean(0): ${Boolean(0)} | Boolean(1): ${Boolean(1)}`);
+
+// Шаблонний рядок
+const name = "Ярослав";
+const age = 20;
+const university = "ХАІ";
+logResult(`Шаблонний рядок: Студент ${name}, вік: ${age} років, університет: ${university}`);
+
+// Порівняння
+logResult(`Різниця між == та ===:
+    10 == "10" : ${10 == "10"} (порівняння значень із приведенням типів)
+    10 === "10": ${10 === "10"} (строге порівняння з урахуванням типу)
+
+    null == undefined  : ${null == undefined} (вони рівні один одному)
+    null === undefined : ${null === undefined} (різні типи: об'єкт/null та undefined)
+
+    0 == ""  : ${0 == ""} (порожня строка до числа 0)
+    0 === "" : ${0 === ""} (Number проти String)`);
